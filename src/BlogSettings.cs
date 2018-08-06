c768beda0b6c513620c1a43cad0fd200582fe8b2
KB Chainer <chainerenator@gmail.com>
2018-08-06 08:30:32 +0300

    some changes basic

--- src/BlogSettings.cs
+++ src/BlogSettings.cs
@@ -3,7 +3,7 @@
     public class BlogSettings
     {
         public string Owner { get; set; } = "The Owner";
-        public int PostsPerPage { get; set; } = 2;
+        public int PostsPerPage { get; set; } = 5;
         public int CommentsCloseAfterDays { get; set; } = 10;
     }
 }
